ec3cbc99398b338e7c6b243a407f2104b72adccb
saketkumar1996 <callmesaket@gmail.com>
2022-11-22 21:13:35 +0530

    Rating Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import {Container} from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+
 
 const App = () => {
   return (
@@ -9,7 +11,7 @@ const App = () => {
       <Header/>
       <main className='py-3'>
       <Container>
-      <h1>Welcome to Proshop</h1>
+      <HomeScreen/>
       </Container>
       </main>
       <Footer/>
