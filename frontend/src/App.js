b6fc2af32b9a86f2ab338edf2baa14b6c6e70592
saketkumar1996 <callmesaket@gmail.com>
2022-11-22 23:35:51 +0530

    React Router and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,28 @@
 import React from 'react'
+// uses HTML5 history API
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import {Container} from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 
 const App = () => {
   return (
-    <div>
+    <Router>
+    
       <Header/>
       <main className='py-3'>
       <Container>
-      <HomeScreen/>
+      <Route path='/' component={HomeScreen} exact/>
+      <Route path='/product/:id' component={ProductScreen}/>
+
       </Container>
       </main>
       <Footer/>
-    </div>
+    
+    </Router>
   )
 }
 
