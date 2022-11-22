dcd1f289311636dbf165bb2c4812ab896f90c1eb
saketkumar1996 <callmesaket@gmail.com>
2022-11-22 13:31:54 +0530

    added bootstrap and basic header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import {Container} from 'react-bootstrap'
 
 const App = () => {
   return (
     <div>
+      <Header/>
+      <main className='py-3'>
+      <Container>
       <h1>Welcome to Proshop</h1>
+      </Container>
+      </main>
+      <Footer/>
     </div>
   )
 }
